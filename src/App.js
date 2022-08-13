5e83eb9a0f23e1cdad1fd10b7ba4dbe18d568461
Juan Oliver <juanoliver995@gmail.com>
2022-08-13 02:28:01 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import 'atropos/css';
+import Atropos from 'atropos/react';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='flip-container'>
+        <Atropos duration={1000} highlight={true} shadow={false}>
+          <div className='card-flip'>
+            <div className='marca-de-agua'><h1>Between Rivers</h1></div>
+            <div className='dorso'>
+              <div className='container'>
+                <div className='container-card'>
+                  <div className='card'>
+                    <div className='header-card' data-atropos-offset="3">
+                      <div className='header-name'>
+                        <div className='logo'></div>
+                        <p>Marianelo</p>
+                      </div>
+                      <div className='header-ticket-number'>
+                        <p>TICKET NUMBER</p>
+                        <p><span># </span>0000</p>
+                      </div>
+                    </div>
+                    <div className='main-card'>
+                      <h1 data-atropos-offset="10" data-atropos-opacity="0.9;1">Marianelo</h1>
+                      <div data-atropos-offset="-2">
+                        <p data-atropos-offset="5">OCTUBRE 01 • GUALEGUAYCHU • 01AM</p>
+                      </div>
+                    </div>
+                    <div className='footer-card' data-atropos-offset="8">
+                      <h3>Invitado Especial</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='frente'>
+              <div className='container'>
+                <div className='container-card' data-atropos-offset="0">
+                  <div className='card-frente'>
+                    <div className='main-card-frente'>
+                      <h1 data-atropos-offset="4">#Birthday Gero</h1>
+                      <div data-atropos-offset="-2">
+                        <p data-atropos-offset="-3">Invitacion Especial</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Atropos>
+      </div>
     </div>
   );
 }
