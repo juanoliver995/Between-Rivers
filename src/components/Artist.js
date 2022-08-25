f23b340352e138e6f66ca8711efd71a347ed1e28
Juan Oliver <juanoliver995@gmail.com>
2022-08-25 02:12:16 -0300

    add lazy loading

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -8,23 +8,23 @@ const Artist = ({ artist }) => {
         <>
             <div className="container-artist">
                 <div className="img-artist">
-                    <img src={artist?.urlImages?.img1} alt={artist.name} />
+                    <img loading="lazy" src={artist?.urlImages?.img1} alt={artist.name} />
                 </div>
                 <div className="info-artist">
                     <h1>{artist.name}</h1>
                     <p>{artist.biografia}</p>
                     <div className="links-container">
-                        {artist?.socialLinks?.soundCloud ? <a target="_blank" rel="noreferrer" href={artist?.socialLinks?.soundCloud}><img src={soundcloud} alt="soundcloud"></img></a> : ""}
-                        {artist?.socialLinks?.youTube ? <a target="_blank" rel="noreferrer" href={artist?.socialLinks?.youTube}><img src={youtube} alt="youtube"></img></a> : ""}
-                        {artist?.socialLinks?.spotify ? <a target="_blank" rel="noreferrer" href={artist?.socialLinks?.spotify}><img src={spotify} alt="spotify"></img></a> : ""}
-                        {artist?.socialLinks?.instagram ? <a target="_blank" rel="noreferrer" href={artist?.socialLinks?.instagram}><img src={instagram} alt="instagram"></img></a> : ""}
+                        {artist?.socialLinks?.soundCloud ? <a target="_blank" rel="noreferrer" href={artist?.socialLinks?.soundCloud}><img loading="lazy" src={soundcloud} alt="soundcloud"></img></a> : ""}
+                        {artist?.socialLinks?.youTube ? <a target="_blank" rel="noreferrer" href={artist?.socialLinks?.youTube}><img loading="lazy" src={youtube} alt="youtube"></img></a> : ""}
+                        {artist?.socialLinks?.spotify ? <a target="_blank" rel="noreferrer" href={artist?.socialLinks?.spotify}><img loading="lazy" src={spotify} alt="spotify"></img></a> : ""}
+                        {artist?.socialLinks?.instagram ? <a target="_blank" rel="noreferrer" href={artist?.socialLinks?.instagram}><img loading="lazy" src={instagram} alt="instagram"></img></a> : ""}
                     </div>
                 </div>
             </div>
             <div className="galeri-container">
                 <div className="galeri">
-                    {artist?.urlImages?.img2 ? <img src={artist?.urlImages?.img2} alt={artist.name}></img> : ""}
-                    {artist?.urlImages?.img3 ? <img src={artist?.urlImages?.img3} alt={artist.name}></img> : ""}
+                    {artist?.urlImages?.img2 ? <img loading="lazy" src={artist?.urlImages?.img2} alt={artist.name}></img> : ""}
+                    {artist?.urlImages?.img3 ? <img loading="lazy" src={artist?.urlImages?.img3} alt={artist.name}></img> : ""}
                 </div>
             </div>
         </>
